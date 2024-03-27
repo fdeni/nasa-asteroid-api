aeee8638f2e2868c9c48a9dcdbdf0018978a89e3
deni <deni@investree.id>
2024-03-27 13:39:38 +0700

    [Deni] - init project

---
+++ src/main/java/com/nasa/asteroid/nasaasteroidapi/NasaAsteroidApiApplication.java
@@ -0,0 +1,13 @@
+package com.nasa.asteroid.nasaasteroidapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaAsteroidApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaAsteroidApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nasa/asteroid/nasaasteroidapi/NasaAsteroidApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nasa.asteroid.nasaasteroidapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaAsteroidApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
